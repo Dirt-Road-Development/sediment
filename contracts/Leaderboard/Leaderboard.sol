751e9b910c5b2b3f480762331721aa4cdd6a2631
TheGreatAxios <sawyer@dirtroad.dev>
2024-05-04 10:05:34 -0700

    Add Documentation for Leaderboard Contracts

--- contracts/Leaderboard/Leaderboard.sol
+++ contracts/Leaderboard/Leaderboard.sol
@@ -3,51 +3,68 @@ pragma solidity ^0.8.25;
 
 import "../Authority/Authority.sol";
 
+/// @title Leaderboard
+/// @author Your Name
+/// @notice A contract to manage a leaderboard of scores associated with Ethereum addresses
+/// @dev This contract inherits from the Authority contract and uses role-based access control
 contract Leaderboard is Authority {
-
+    /// @dev Struct representing a user entry in the leaderboard
     struct User {
-        address user;
-        uint64 highScore;
-        uint64 timestamp;
-        uint32 index;
+        address user; // The user's Ethereum address
+        uint64 highScore; // The user's high score
+        uint64 timestamp; // The timestamp when the score was submitted
+        uint32 index; // The index of the user in the sorted leaderboard
     }
 
-    uint32 resetIndex;
-    User[] public leaderboard;
-    uint32 public maxLength;
+    uint32 resetIndex; // Variable used for incremental reset
+    User[] public leaderboard; // The leaderboard array of User structs
+    uint32 public maxLength; // The maximum length of the leaderboard
+    bool public paused; // Flag indicating whether score submission is paused
 
-    bool public paused;
+    event IncrementalReset(uint32 indexed amount); // Event emitted when an incremental reset is performed
+    event Reset(); // Event emitted when the leaderboard is completely reset
+    event SubmitScore(address indexed user, uint64 indexed highScore); // Event emitted when a score is submitted but not added to the leaderboard
+    event SubmitScoreAndAdd(address indexed user, uint64 indexed highScore); // Event emitted when a score is submitted and added to the leaderboard
 
-    event IncrementalReset(uint32 indexed amount);
-    event Reset();
-    event SubmitScore(address indexed user, uint64 indexed highScore);
-    event SubmitScoreAndAdd(address indexed user, uint64 indexed highScore);
-
+    /// @notice Constructor to initialize the contract
+    /// @param _maxLength The maximum length of the leaderboard
     constructor(uint32 _maxLength) {
         maxLength = _maxLength;
         paused = false;
     }
 
+    /// @notice Submit a new high score for a user
+    /// @param user The Ethereum address of the user
+    /// @param highScore The new high score to be submitted
+    /// @dev Only callable by the SERVER_ROLE
     function submitScore(address user, uint64 highScore) external onlyRole(SERVER_ROLE) {
         if (paused) revert("Submitted Scores is Paused");
         if (length() >= maxLength && highScore <= leaderboard[length() - 1].highScore) {
             emit SubmitScore(user, highScore);
-            return;   
+            return;
         }
-
         _addToLeaderboard(user, highScore, length() >= maxLength ? length() - 1 : length());
         _sort(leaderboard, 0, int32(length()));
     }
 
+    /// @notice Get the current length of the leaderboard
+    /// @return The length of the leaderboard
     function length() public view returns (uint32) {
         return uint32(leaderboard.length);
     }
 
+    /// @dev Internal function to add a new user to the leaderboard
+    /// @param user The Ethereum address of the user
+    /// @param highScore The new high score to be added
+    /// @param index The index at which the new user should be inserted
     function _addToLeaderboard(address user, uint64 highScore, uint32 index) internal {
         leaderboard.push(User(user, highScore, uint64(block.timestamp), index));
         emit SubmitScoreAndAdd(user, highScore);
     }
 
+    /// @notice Reset the entire leaderboard
+    /// @dev Only callable by the MANAGER_ROLE
+    /// @dev Will revert if the leaderboard length is greater than 25,000
     function reset() external onlyRole(MANAGER_ROLE) {
         if (length() < 25_000) {
             delete leaderboard;
@@ -56,25 +73,28 @@ contract Leaderboard is Authority {
         revert("Reset must be done in increments");
     }
 
+    /// @notice Perform an incremental reset of the leaderboard
+    /// @dev Only callable by the MANAGER_ROLE
+    /// @dev Removes up to 1,500 entries from the leaderboard
     function incrementalReset() external onlyRole(MANAGER_ROLE) {
         if (!paused) paused = true;
         uint32 removalAmount = length() > 1500 ? 1500 : length();
         for (uint32 i = 0; i < removalAmount; i++) {
             leaderboard.pop();
         }
-
         emit IncrementalReset(removalAmount);
     }
 
+    /// @dev Internal function to sort the leaderboard array using the quicksort algorithm
+    /// @param arr The leaderboard array to be sorted
+    /// @param left The left index of the subarray to be sorted
+    /// @param right The right index of the subarray to be sorted
     function _sort(User[] memory arr, int256 left, int256 right) internal {
-        
         int256 i = left;
         int256 j = right;
-        
         if (i == j) return;
-        
-        uint256 pivot = arr[uint256(left + (right - left) / 2)].index;
 
+        uint256 pivot = arr[uint256(left + (right - left) / 2)].index;
         while (i <= j) {
             while (arr[uint256(i)].index > pivot) i++;
             while (pivot > arr[uint256(j)].index) j--;
@@ -84,6 +104,7 @@ contract Leaderboard is Authority {
                 j--;
             }
         }
+
         if (left < j)
             _sort(arr, left, j);
         if (i < right)
