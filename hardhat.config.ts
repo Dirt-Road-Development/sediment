2a2025889f15fd25a7d28e7d1f26ea7709b47500
TheGreatAxios <sawyer@dirtroad.dev>
2024-05-03 21:01:28 -0700

    Add Web3Leaderboard

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-deploy";
-import "hardhat-deploy-ethers";
 import "solidity-coverage";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.25",
+  solidity: "0.8.25"
 };
 
 export default config;
