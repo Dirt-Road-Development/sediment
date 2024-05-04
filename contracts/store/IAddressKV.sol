7ba95ed8b28f99cba8efd6d54ec8cdb0a579c82b
TheGreatAxios <sawyer@dirtroad.dev>
2024-05-03 20:06:11 -0700

    Remove v0 Modules

--- contracts/store/IAddressKV.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IAddressKV {
-    function get(address key) external view returns (bytes memory);
-    function remove(address key) external;
-    function set(address key, bytes calldata value) external;
-}
--- files/history_external_uri_3.js
+++
@@ -1,9 +0,0 @@
-async function main() {
-    console.log("I am a JavaScript file");
-}
-
-main()
-    .catch((err) => {
-        console.error(err);
-        process.exitCode = 1;
-    })
