1ca195716633f5cb93794920a1183dcb11883a0e
TheGreatAxios <sawyer@dirtroad.dev>
2024-05-04 14:14:27 -0700

    Update File Paths

--- contracts/Leaderboard/BytesLeaderboard.sol
+++ contracts/Leaderboard/BytesLeaderboard.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import "../Authority/Authority.sol";
+import "../authority/Authority.sol";
 
 /// @title BytesLeaderboard
 /// @author Dirt Road Development
